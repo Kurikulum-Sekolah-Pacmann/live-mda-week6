a87dc64d4e92135df0d1c37b80c9dd7071ead8ac
rahilaode <laoderahilatansa@gmail.com>
2025-03-15 06:16:03 +0700

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Start all airflow services
-# docker compose -f ./setup/airflow/docker-compose.yml down -v
+docker compose -f ./setup/airflow/docker-compose.yml down -v
 docker compose -f ./setup/airflow/docker-compose.yml up --build --detach --force-recreate
 
 # Start monitoring services
-# docker compose -f ./setup/airflow-monitoring/docker-compose.yml down -v
+docker compose -f ./setup/airflow-monitoring/docker-compose.yml down -v
 docker compose -f ./setup/airflow-monitoring/docker-compose.yml up --build --detach --force-recreate
 
 # Import airflow variables and connections
@@ -13,13 +13,13 @@ docker exec -it airflow-webserver airflow connections import /init/variables_and
 # docker exec -it airflow-webserver airflow variables import -a overwrite /init/variables_and_connections/airflow_variables_init.json
 
 # Start data lake services
-# docker compose -f ./setup/data-lake/docker-compose.yml down -v
+docker compose -f ./setup/data-lake/docker-compose.yml down -v
 docker compose -f ./setup/data-lake/docker-compose.yml up --build --detach --force-recreate
 
 # Start data sources services
-# docker compose -f ./setup/sources/docker-compose.yml down -v
+docker compose -f ./setup/sources/docker-compose.yml down -v
 docker compose -f ./setup/sources/docker-compose.yml up --build --detach --force-recreate
 
 # Start data spark services
-# docker compose -f ./setup/spark/docker-compose.yml down -v
+docker compose -f ./setup/spark/docker-compose.yml down -v
 docker compose -f setup/spark/docker-compose.yml up --build --detach --force-recreate
